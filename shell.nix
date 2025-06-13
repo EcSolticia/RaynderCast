a1d358f2401fc735782907763ffa4eb259338551
EcSolticia <ecsolticia@gmail.com>
2025-06-14 01:48:38 +0600

    nix-shell: Add VSCode environment

--- shell.nix
+++ shell.nix
@@ -1,6 +1,15 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
+  pkgs = import nixpkgs { config = {
+    allowUnfree = true;
+  }; overlays = []; };
+
+  vscode-with-extensions = pkgs.vscode-with-extensions.override {
+    vscodeExtensions = with pkgs.vscode-extensions; [
+      ms-vscode.cpptools
+      ms-vscode.cmake-tools
+    ];
+  };
 in
 
 pkgs.mkShellNoCC {
@@ -10,5 +19,6 @@ pkgs.mkShellNoCC {
     gnumake
     cmake
     SDL2
+    vscode-with-extensions
   ];
 }
